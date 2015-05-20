74a39c158a247400ca2f97c452b8daf05a11cfe0
Brian Knorr <btknorr@gmail.com>
2015-05-20 13:29:23 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var execSync = require('sync-exec');
+
+module.exports = function(path) {
+    return function(req, res, next) {
+        var build = 'cd ' + path + ' && sudo ulimit -n 10000 && duo --use ../../node_modules/duo-jade --copy index.{js,css}'
+        console.info(execSync(build));
+        next();
+    };
+};
