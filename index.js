2af8b76c30f856d0d721507422bcf77d2fb6afb9
Brian Knorr <btknorr@gmail.com>
2015-06-03 11:07:18 -0500

    only output stderr

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var execSync = require('sync-exec');
 module.exports = function(path) {
     return function(req, res, next) {
         var build = 'cd ' + path + ' && sudo ulimit -n 10000 && duo --use ../../node_modules/duo-jade --copy index.{js,css}'
-        console.info(execSync(build));
+        console.info(execSync(build).stderr);
         next();
     };
 };
